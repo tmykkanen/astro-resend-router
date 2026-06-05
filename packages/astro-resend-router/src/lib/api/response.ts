7f58e1a46c1c15757733c4c49fadc54ff9fef397
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2026-06-05 16:39:03 -0500

    make json responses more explicit

--- packages/astro-resend-router/src/lib/api/response.ts
+++ packages/astro-resend-router/src/lib/api/response.ts
@@ -3,6 +3,7 @@ import type { Status } from "#/lib/shared/types.ts";
 import { log } from "./logging.ts";
 
 type JSONResponse<T = unknown> = {
+	code: string;
 	message: string;
 	data?: T;
 };
@@ -23,9 +24,13 @@ const jsonResponse = <T>(payload: JSONResponse<T>, status: number) => {
  * Error response (logs + HTTP response)
  */
 export const errorResponse = <C extends string>(error: Status<C>) => {
-	log(error.message, "error");
+	log(`code: ${error.code} | message: ${error.message}`, "error");
 	return jsonResponse(
-		{ message: error.message, data: error.details ?? "" },
+		{
+			code: error.code,
+			message: error.message,
+			...(error.details && { data: error.details }),
+		},
 		error.statusCode,
 	);
 };
@@ -34,6 +39,13 @@ export const errorResponse = <C extends string>(error: Status<C>) => {
  * Success response (logs + HTTP response)
  */
 export const successResponse = <C extends string>(value: Status<C>) => {
-	log(value.message, "log");
-	return jsonResponse({ message: value.message }, value.statusCode);
+	log(`code: ${value.code} | message: ${value.message}`, "log");
+	return jsonResponse(
+		{
+			code: value.code,
+			message: value.message,
+			...(value.details && { data: value.details }),
+		},
+		value.statusCode,
+	);
 };
