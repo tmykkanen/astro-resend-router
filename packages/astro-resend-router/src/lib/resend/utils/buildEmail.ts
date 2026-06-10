fb7f12b09f4aafb15ba388f8ae3e59d5062f24ea
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2026-06-10 11:01:28 -0500

    feat: added support for mailchimp forwarding - use cheerio to strip mailchimp header and footer, since we'll use   Resend unsubscribe links

--- packages/astro-resend-router/src/lib/resend/utils/buildEmail.ts
+++ packages/astro-resend-router/src/lib/resend/utils/buildEmail.ts
@@ -1,23 +1,33 @@
+import * as cheerio from "cheerio";
+
 import type { ValidatedContext } from "#/lib/shared/types.ts";
 
 export const buildEmail = (content: string, ctx: ValidatedContext) => {
+	// Strip Mailchimp Header & Footer
+	let body = content;
+	if (body) {
+		const $ = cheerio.load(body);
+		$("#templatePreheader").remove();
+		$("#canspamBarWrapper").remove();
+
+		body = $.html();
+	}
+
 	const footer = ctx.segment.customEmailFooter || defaultEmailFooter(ctx);
-	return `${content} ${footer}`;
+	return `${body} ${footer}`;
 };
 
 const defaultEmailFooter = (ctx: ValidatedContext) => {
 	return `
-  <hr style="margin-top:24px;border:none;border-top:1px solid #444;" />
-
+  <br>
+  <br>
+  <center>
   <p style="font-size:12px; color:#666; line-height:1.5; margin-top:16px;">
         You’re receiving this because you subscribed to ${
 					ctx.segment.segmentName
 				}${ctx.topic ? ` and ${ctx.topic.topicName}` : ""}.<br />
-        Want to stop receiving these emails?<br />
-
-        <a href="{{{RESEND_UNSUBSCRIBE_URL}}}" style="color:#666;">
-          Unsubscribe instantly.
-        </a>
+        Want to stop receiving these emails? <a href="{{{RESEND_UNSUBSCRIBE_URL}}}" style="color:#666;">Click here to unsubscribe.</a>
   </p>
+  </center>
   `;
 };
